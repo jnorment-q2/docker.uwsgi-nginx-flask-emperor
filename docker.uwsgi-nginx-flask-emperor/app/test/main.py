2426480816096afd040e2c0c6e69025e633ffed4
jnorment.q2 <jnorment.q2@gmail.com>
2017-12-14 19:34:38 +0000

    Updated to use python 3.5.

--- docker.uwsgi-nginx-flask-emperor/app/test/main.py
+++ docker.uwsgi-nginx-flask-emperor/app/test/main.py
@@ -1,12 +1,14 @@
 # encoding=UTF-8
 #!flask/bin/python
 
+import platform
+
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def index():
-	return "Hello, this is test 1~"
+	return "Hello, this is test 1 running in {}~".format(platform.python_version())
       
 if __name__ ==  '__main__':
 	app.run(debug=True)      
