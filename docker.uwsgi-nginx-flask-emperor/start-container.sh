76a19210f10acf3c59d38b93a06d800692b90836
Cognitiaclaeves <cognitiaclaeves@gmail.com>
2018-01-04 02:34:43 +0000

    Allow config files to be changed interactively.

---
+++ docker.uwsgi-nginx-flask-emperor/start-container.sh
@@ -0,0 +1,10 @@
+# logs are now accessible outside of docker container in ./logs
+# and inside docker container at /tmp/appd/logs
+rm logs/*
+# sudo docker run -it -v `pwd`/logs:/tmp/appd/logs:z -p 80:80 testjae-appd:3
+# can also curl port 80 outside of container
+# sudo docker run -it -v `pwd`/logs:/tmp/appd/logs:z -v `pwd`/supervisord:/etc/supervisor:z -v `pwd`/nginx/conf.d:/etc/nginx/conf.d:z -v `pwd`/uwsgi:/etc/uwsgi:z -p 80:80 testjae-appd:3 bash
+echo run this: /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
+sudo docker run -it -v `pwd`/logs:/tmp/appd/logs:z -v `pwd`/supervisord:/etc/supervisor:z -v `pwd`/nginx/conf.d:/etc/nginx/conf.d:z -v `pwd`/uwsgi:/etc/uwsgi:z -p 80:80 testjae-appd:3 bash
+
+
